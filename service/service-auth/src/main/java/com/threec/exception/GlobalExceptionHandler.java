330b03de577dab4fa84d118d80562489e23fe6ed
Laven <jotian@deloitte.com.cn>
2024-07-21 20:53:56 +0800

    feat: 全局异常处理

--- service/service-auth/src/main/java/com/threec/exception/GlobalExceptionHandler.java
+++ service/service-auth/src/main/java/com/threec/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.threec.exception;
 
 import com.threec.tools.exception.BusinessException;
 import com.threec.tools.utils.R;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,6 +21,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleConversionProblem(BusinessException ex) {
         return new ResponseEntity<>(R.error(ex.getCode(), ex.getMsg()), HttpStatus.BAD_REQUEST);
     }
-
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex) {
+        return new ResponseEntity<>(R.error(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
 
 }
