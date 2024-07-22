02d1681d0df0a5004582d84d0acfd8c363e7bba0
Laven <jotian@deloitte.com.cn>
2024-07-22 17:55:57 +0800

    feat: redis常量

---
+++ service/service-auth/src/main/java/com/threec/constant/RedisConstant.java
@@ -0,0 +1,11 @@
+package com.threec.constant;
+
+/**
+ * Redis 常数
+ *
+ * @author Tian, Laven Yongxing
+ * @date 2024/07/22 17:55
+ */
+public class RedisConstant {
+    public static final String SYS_USER = "ThreeC:User:Login:";
+}
