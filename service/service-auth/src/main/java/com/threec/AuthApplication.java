243997140f44fa5e3a59edefda156e1deff5f36d
Laven <jotian@deloitte.com.cn>
2024-07-23 17:56:38 +0800

    feat: 添加jasypt 对配置进行加密

--- service/service-auth/src/main/java/com/threec/AuthApplication.java
+++ service/service-auth/src/main/java/com/threec/AuthApplication.java
@@ -1,11 +1,13 @@
 package com.threec;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com.threec.mybatis.*"))
 public class AuthApplication {
     public static void main(String[] args) {
